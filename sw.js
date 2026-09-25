671a7f4910e1240d02d1f246d3ea4faf91e1345e
binghaoy <binghaoy@users.noreply.github.com>
2026-09-25 18:00:42 +0800

    update 0925-1800

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='tp-ced11a46';
+const V='tp-57b2f6df';
 const CORE=['./','index.html','booking.html','leaflet.css','leaflet.js','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
 const TILE='tp-tiles';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)))});
