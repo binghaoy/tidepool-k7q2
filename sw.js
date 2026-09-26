a73028e81e2f3f48d5c3c4fca63fa56ce03a44c8
binghaoy <binghaoy@users.noreply.github.com>
2026-09-26 21:00:36 +0800

    update 0926-2100

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const V='tp-163323cf';
-const CORE=['./','index.html','booking.html','leaflet.css','leaflet.js','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
+const V='tp-ba45f7ee';
+const CORE=['./','index.html','booking.html','leaflet.css','leaflet.js','manifest.json','icon-180.png','icon-192.png','icon-512.png','photos/p00.jpg','photos/p01.jpg','photos/p02.jpg','photos/p03.jpg','photos/p04.jpg','photos/p05.jpg','photos/p06.jpg','photos/p07.jpg','photos/p08.jpg','photos/p09.jpg','photos/p10.jpg','photos/p11.jpg','photos/p12.jpg','photos/p13.jpg','photos/p14.jpg','photos/p15.jpg','photos/p16.jpg','photos/p17.jpg','photos/p18.jpg','photos/p19.jpg','photos/p20.jpg','photos/p23.jpg','photos/p25.jpg','photos/p26.jpg','photos/p27.jpg','photos/p28.jpg','photos/p29.jpg','photos/p30.jpg','photos/p31.jpg'];
 const TILE='tp-tiles';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V&&k!==TILE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
